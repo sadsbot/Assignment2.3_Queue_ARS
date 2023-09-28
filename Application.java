6f9f7c35f89f12b8b429bc3ef3b76fcf428a4ba5
sadsbot <andrewrosssermons@gmail.com>
2023-09-28 14:16:17 -0400

    first

---
+++ Application.java
@@ -0,0 +1,20 @@
+/**
+* Class Description
+*
+* @author aross-sermons
+* @version 1.0
+* CS215; Lab #
+* Fall 2023
+*/
+
+public class Application {
+
+	/**
+	 *
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
